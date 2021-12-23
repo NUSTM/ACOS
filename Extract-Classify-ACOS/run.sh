c7c8e3f4f5d52410eaccd55628f9928aa1699e7d
Hongjie Cai <hjcai@njust.edu.cn>
2021-12-23 20:53:54 +0800

    Update run.sh

--- Extract-Classify-ACOS/run.sh
+++ Extract-Classify-ACOS/run.sh
@@ -1,6 +1,6 @@
 BERT_BASE_DIR=/mnt/nfs-storage-titan/BERT/uncased_L-12_H-768_A-12
 BASE_DIR=/mnt/nfs-storage-titan/BERT/pytorch_pretrained_BERT
-DATA_DIR=$BASE_DIR/ACOS-main/Extrct-Classify-ACOS
+DATA_DIR=$BASE_DIR/ACOS-main/Extract-Classify-ACOS
 TASK_NAME=quad
 MODEL=quad
 DOMAIN=rest16
